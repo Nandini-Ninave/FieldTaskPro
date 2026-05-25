2ae5ef211f4b296480b59299db9a7c53910922f1
Nandini_Ninave <nandinininave99@gmail.com>
2026-05-25 16:53:08 +0530

    Fixed Issue

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,10 @@ mongoose.connect(process.env.DB_URL).then(()=>{
     console.log(err);
 })
 let app=express()
-app.use(cors())
+app.use(cors({
+    origin:process.nextTick.FRONTEND_URL,
+    credentials:true
+}))
 app.use(express.json())
 // app.use(express.urlencoded({extended:true}))
 app.use("/admin",adminrt)
